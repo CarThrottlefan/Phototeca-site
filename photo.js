5f9c2758ec97808e936ab2a1c087858c5fc465e9
CarThrottlefan <alexandervici@gmail.com>
2023-01-22 05:36:02 +0100

    correct-only form submission

--- photo.js
+++ photo.js
@@ -1,19 +1,70 @@
-let authorValid = false;
+/*let authorValid = false;
 let imageValid = false;
 let altValid = false;
 let descriptionValid = false;
-let tagsValid = false;
+let tagsValid = false;*/
 let submitValid = false;
 
 async function validateSubmission(event)
 {
-    form.author.style.backgroundColor = "black";
     
     if(form.author.value === "")
     {
-        form.author.style.backgroundColor = "red";
+        form.author.style.backgroundColor = "#e63d3d";
+        submitValid = false;
     }
-    event.preventDefault();
+    else
+    {
+        form.author.style.backgroundColor = "white";
+        submitValid = true;
+    }
+
+    if(form.image.value === "")
+    {
+        form.image.style.backgroundColor = "#e63d3d";
+        submitValid = false;
+    }
+    else
+    {
+        form.image.style.backgroundColor = "white";
+        submitValid = true;
+    }
+
+    if(form.description.value === "")
+    {
+        form.description.style.backgroundColor = "#e63d3d";
+        submitValid = false;
+    }
+    else
+    {
+        form.description.style.backgroundColor = "white";
+        submitValid = true;
+    }
+
+    if(form.alt.value === "")
+    {
+        form.alt.style.backgroundColor = "#e63d3d";
+        submitValid = false;
+    }
+    else
+    {
+        form.alt.style.backgroundColor = "white";
+        submitValid = true;
+    }
+
+    if(form.tags.value === "")
+    {
+        form.tags.style.backgroundColor = "#e63d3d";
+        submitValid = false;
+    }
+    else
+    {
+        form.tags.style.backgroundColor = "white";
+        submitValid = true;
+    }
+    
+    if(!submitValid)
+        {event.preventDefault();}
 }
 
 let form = document.getElementById("submitForm");
